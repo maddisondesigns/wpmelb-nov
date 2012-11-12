69f5e34c87d0f0bf4ed1e5001d2db5473917848f
Anthony Hortin <anthony@maddisondesigns.com>
2012-11-13 00:49:27 +1100

    Updated twentytwelve_entry_meta() function to show 'genre' taxonomy when displaying our test 'movie' Custom Post Type

--- functions.php
+++ functions.php
@@ -10,6 +10,72 @@
 
 
 /**
+ * I updated the default twentytwelve_entry_meta() function to display taxonomy data when dislaying our Custom Post Type
+ * You can simply replace the original code with this code
+ */
+if ( ! function_exists( 'twentytwelve_entry_meta' ) ) :
+/**
+ * Prints HTML with meta information for current post: categories, tags, permalink, author, and date.
+ *
+ * Create your own twentytwelve_entry_meta() to override in a child theme.
+ *
+ * @since Twenty Twelve 1.0
+ */
+function twentytwelve_entry_meta() {
+	if( is_post_type_archive( 'movie' ) ) {
+		$tax = get_the_term_list( $post->ID, 'genre', '', ', ','' );
+	}
+	else {
+		// Translators: used between list items, there is a space after the comma.
+		$categories_list = get_the_category_list( __( ', ', 'twentytwelve' ) );
+	}
+
+	// Translators: used between list items, there is a space after the comma.
+	$tag_list = get_the_tag_list( '', __( ', ', 'twentytwelve' ) );
+
+	$date = sprintf( '<a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a>',
+		esc_url( get_permalink() ),
+		esc_attr( get_the_time() ),
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html( get_the_date() )
+	);
+
+	$author = sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span>',
+		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+		esc_attr( sprintf( __( 'View all posts by %s', 'twentytwelve' ), get_the_author() ) ),
+		get_the_author()
+	);
+
+	// Translators: 1 is category, 2 is tag, 3 is the date and 4 is the author's name.
+	if ( $tag_list ) {
+		$utility_text = __( 'This entry was posted in %1$s and tagged %2$s on %3$s<span class="by-author"> by %4$s</span>.', 'twentytwelve' );
+	} elseif ( $categories_list ) {
+		$utility_text = __( 'This entry was posted in %1$s on %3$s<span class="by-author"> by %4$s</span>.', 'twentytwelve' );
+	} elseif ( $tax ) {
+		$utility_text = __( 'This entry was posted in %1$s on %3$s<span class="by-author"> by %4$s</span>.', 'twentytwelve' );
+	} else {
+		$utility_text = __( 'This entry was posted on %3$s<span class="by-author"> by %4$s</span>.', 'twentytwelve' );
+	}
+
+	printf(
+		$utility_text,
+		(  is_post_type_archive( 'movie' ) ? $tax : $categories_list ),
+		$tag_list,
+		$date,
+		$author
+	);
+}
+endif;
+
+
+
+/**
+ * The code below is new code that has been added to our file for creating the test Custom Post Type and out pre_get_posts hook
+ */
+
+
+
+/**
  * Add an action when WP Admin initialises to call the function my_create_custom_post_types() to register our Custom Post Type
  */
 function my_create_custom_post_types() {
